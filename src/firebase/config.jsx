dbe1c2699a4bb31675b5710ec8d4d399a325594a
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2022-05-15 15:18:46 +0100

    firbase initialization

---
+++ src/firebase/config.jsx
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBGzXKtfVd_ii_Ha40pCoKmS2PePaOAMYc',
+  authDomain: 'taskit-v1.firebaseapp.com',
+  projectId: 'taskit-v1',
+  storageBucket: 'taskit-v1.appspot.com',
+  messagingSenderId: '220184554283',
+  appId: '1:220184554283:web:98361afb70be66a459736e'
+}
+
+// INITIALIZE FIREBASE
+firebase.initializeApp(firebaseConfig)
+
+// INITIALIZE SERVICES
+const taskitFirestore = firebase.firestore()
+const taskitAuth = firebase.auth()
+
+// FIREBASE TIME STAMP
+const timestamp = firebase.firestore.Timestamp
+
+export { taskitFirestore, taskitAuth, timestamp }
