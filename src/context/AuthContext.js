cd756cf60c8514fccc1e7b7c5da5bc515d43e719
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2022-05-19 17:15:54 +0100

    added firbase init

---
+++ src/context/AuthContext.js
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types'
+import { createContext, useReducer, useEffect } from 'react'
+import { taskitAuth } from '../firebase/config'
+
+export const AuthContext = createContext()
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return { ...state, user: action.payload }
+    case 'LOGOUT':
+      return { ...state, user: null }
+    case 'AUTH_IS_READY':
+      return { user: action.payload, authIsReady: true }
+    default:
+      return state
+  }
+}
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authIsReady: false
+  })
+
+  useEffect(() => {
+    const unsub = taskitAuth.onAuthStateChanged((user) => {
+      dispatch({ type: 'AUTH_IS_READY', payload: user })
+      unsub()
+    })
+  }, [])
+
+  console.log('AuthContext state:', state)
+
+  return <AuthContext.Provider value={{ ...state, dispatch }}>{children}</AuthContext.Provider>
+}
+
+// AuthContextProvider PROPTYPES
+AuthContextProvider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired
+}
---
+++ src/hooks/useLogout.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import { taskitAuth } from '../firebase/config'
+import { useAuthContext } from './useAuthContext'
+
+export const useLogout = () => {
+  const [isCancelled, setIsCancelled] = useState(false)
+  const [error, setError] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const { dispatch } = useAuthContext()
+
+  const logout = async () => {
+    setError(null)
+    setIsPending(true)
+
+    try {
+      // sign the user out
+      await taskitAuth.signOut()
+
+      // dispatch logout action
+      dispatch({ type: 'LOGOUT' })
+
+      // update state
+      if (!isCancelled) {
+        setIsPending(false)
+        setError(null)
+      }
+    } catch (err) {
+      if (!isCancelled) {
+        setError(err.message)
+        setIsPending(false)
+      }
+    }
+  }
+
+  useEffect(() => {
+    return () => setIsCancelled(true)
+  }, [])
+
+  return { logout, error, isPending }
+}
